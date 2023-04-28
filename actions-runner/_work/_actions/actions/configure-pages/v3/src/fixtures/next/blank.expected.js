cbc44e589d059b05e92d9ba027a5c8d98fe3c3d8
Theuins Lombard <theunis.lombard@kidsindustries.com>
2023-04-28 15:01:27 +0200

    removed runner files

--- actions-runner/_work/_actions/actions/configure-pages/v3/src/fixtures/next/blank.expected.js
+++
@@ -1,3 +0,0 @@
-// Default Pages configuration for Next
-const nextConfig = { images: { unoptimized: true }, experimental: { images: { unoptimized: true } }, basePath: '/docs' }
-module.exports = nextConfig
